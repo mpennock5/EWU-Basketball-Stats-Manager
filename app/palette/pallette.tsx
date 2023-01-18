712ac286f7a347f7d6bf502f182ec60efecd3258
roydunn <roydunn100@gmail.com>
2023-01-18 22:01:38 +0000

    scaffolding for project

---
+++ app/palette/pallette.tsx
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#b7142e", // red
+    },
+    secondary: {
+      main: "#333333", // dark grey
+    },
+  },
+});
+export default theme;
