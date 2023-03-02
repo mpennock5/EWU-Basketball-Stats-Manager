78134910768ebaf25f43940660910e8ea51eb487
roydunn19 <roydunn19@github.com>
2023-03-02 02:28:10 +0000

    added folder structure and removed unused code

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,19 +1,7 @@
-import { AppBar } from '@mui/material';
-import { Link } from '@remix-run/react';
-
 export default function Index() {
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-      <AppBar sx={{ backgroundColor: '#A10022' }}>
-        <Link to={'/index'}>
-          {/* Temporary: need to be replaced with local image */}
-          <img
-            style={{ width: '50px' }}
-            src="https://cdn.ewu.edu/wp-content/themes/ewu-2018/assets/ewuEagle.png"
-            alt="logo"
-          ></img>
-        </Link>
-      </AppBar>
-    </div>
+    <div
+      style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}
+    ></div>
   );
 }
