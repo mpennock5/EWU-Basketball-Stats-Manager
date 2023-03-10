8d31961662dba8ad7d3ba2f628a67796f4e76b5e
bfristad <benjaminfristad@gmail.com>
2023-03-10 05:00:48 +0000

    made home banner resizable and paramitized EWU LOGO

--- app/components/HomeBanner.tsx
+++ app/components/HomeBanner.tsx
@@ -1,32 +1,26 @@
-import { Typography } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 import EwuLogo from './EwuLogo';
 
 const HomeBanner = (props: { children?: JSX.Element | JSX.Element[] }) => {
   const { children } = props;
   return (
-    <div
+    <Stack
+      direction="row"
+      justifyContent="space-between"
+      height="auto"
+      p={2}
       style={{
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        height: '132px',
-        backgroundImage: 'linear-gradient(to right, #b7142e, #5e0816',
+        background: 'linear-gradient(to right, #b7142e, #5e0816',
       }}
     >
-      <div
-        style={{ display: 'flex', flexDirection: 'column', paddingLeft: '3em' }}
-      >
+      <Box pt={3}>
         <Typography variant="h3" color="text.secondary">
           EASTERN WASHINGTON UNIVERSITY
         </Typography>
-        <div style={{ display: 'flex', gap: '1em', paddingTop: '4px' }}>
-          {children}
-        </div>
-      </div>
-      <div style={{ paddingRight: '1em' }}>
-        <EwuLogo />
-      </div>
-    </div>
+        {children}
+      </Box>
+      <EwuLogo />
+    </Stack>
   );
 };
 export default HomeBanner;
